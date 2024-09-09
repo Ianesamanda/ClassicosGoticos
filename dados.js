8dfb61b9d4a11e6e7ddd2aced1c648eac5fde62e
Amanda <156706364+Ianesamanda@users.noreply.github.com>
2024-09-09 19:32:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Drácula",
+      descricao: "Publicado em 1897, Drácula, de Bram Stoker, é um clássico da literatura gótica que nos transporta para um mundo de mistério e terror. A história, narrada em forma de diário, acompanha um grupo de pessoas que lutam contra o poderoso Conde Drácula, um vampiro ancestral que aterroriza a Inglaterra vitoriana. A obra, repleta de elementos sobrenaturais e atmosferas sombrias, consolidou o mito do vampiro na cultura popular e continua a fascinar leitores até hoje. ",
+      link: "https://pt.wikipedia.org/wiki/Dr%C3%A1cula",
+      tags:"terror gótico ficção histórica vampiro sobrenatural medo morte tenebrosa "
+    },
+    {
+      titulo: "Carmilla",
+      descricao: "Carmilla, de Sheridan Le Fanu, é considerada uma das primeiras histórias de vampiros a explorar a sexualidade e o erotismo. Publicado antes de Drácula, Carmilla introduziu elementos que se tornaram icônicos no gênero vampírico.",
+      link: "https://www.bakhall.com/artikel/carmilla",
+      tags:" romance gótico vampiro sexualidade erotismo sombria sensual misteriosa lésbico"
+    },
+    {
+      titulo: "Frankenstein",
+      descricao: "Frankenstein, de Mary Shelley, é um clássico da literatura gótica que explora temas como a criação da vida artificial e as consequências éticas da ciência. A história do cientista Victor Frankenstein e sua criatura aterrorizante continua a fascinar leitores até hoje.",
+      link: "https://pt.wikipedia.org/wiki/Frankenstein",
+      tags:"ficção científica gótico criação artificial ética ciência vs. natureza isolamento monstro sombria melancólica filosófica"
+    }
+  ];
